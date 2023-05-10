b5ef66bc4c470a6b8bb09b63de6399b451745d65
MFitzgerald89 <122069535+MFitzgerald89@users.noreply.github.com>
2023-05-09 22:31:14 -0400

    adds matches index functionality to matches.jsx

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { Home } from "./Home";
 import { About } from "./About";
 import { Matches } from "./Matches";
@@ -5,14 +6,27 @@ import { Signup } from "./Signup";
 import { Login } from "./Login";
 import { LogoutLink } from "./LogoutLink";
 import { Routes, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 export function Content() {
+  const [users, setUsers] = useState([]);
+
+  const handleIndexUsers = () => {
+    console.log("handleIndexusers");
+    axios.get("http://localhost:3000/users.json").then(response => {
+      console.log(response.data);
+      setUsers(response.data);
+    });
+  };
+
+  useEffect(handleIndexUsers, []);
+
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/matches" element={<Matches />} />
+        <Route path="/matches" element={<Matches users={users} />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/logoutlink" element={<LogoutLink />} />
--- src/Matches.jsx
+++ src/Matches.jsx
@@ -1,3 +1,14 @@
-export function Matches() {
-  return <h1>Matches</h1>;
+export function Matches(props) {
+  return (
+    <div>
+      {props.users.map(user => (
+        <div key={user.id}>
+          <h1>{user.first_name}</h1>
+          <p>{user.age}</p>
+          <p>{user.orientation}</p>
+          <p>{user.photos}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
