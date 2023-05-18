4095ed45b6469daa4247c47c5d6171b83d27ee0d
MFitzgerald89 <122069535+MFitzgerald89@users.noreply.github.com>
2023-05-18 17:19:36 -0400

    updates main.jsx file with reactdom

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./App";
-import "./index.css";
-import "./App.scoped.scss";
-import "./Home.scoped.scss";
+// import "./index.css";
+// import "./App.scoped.scss";
+// import "./Home.scoped.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
